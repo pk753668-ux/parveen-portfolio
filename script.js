b6097f9cb36c65da199e72d83c0b1c957f81973d
Parveen Kumar <pk753668@gmail.com>
2026-02-19 07:36:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-// Smooth scrolling for nav links
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if(target) target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
 function toggleMenu() {
   document.getElementById("navLinks").classList.toggle("show");
-}
-if (localStorage.getItem("theme") === "light") {
-  document.body.classList.add("light");
 }
 
 function toggleTheme() {
   document.body.classList.toggle("light");
-  localStorage.setItem(
-    "theme",
-    document.body.classList.contains("light") ? "light" : "dark"
-  );
 }
